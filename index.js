ac8d5627e6419536f30d50ae0c7210254db446ca
Aradhna Nigam <abhayk6190@gmail.com>
2025-02-19 02:33:34 +0530

    Added notes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+const path = require("path"); //this is path module used to handle directories and paths
 const urlRouter = require("./routes/url");
 
 const {connectMongoDb} = require("./connection");
@@ -10,8 +10,8 @@ connectMongoDb("mongodb://127.0.0.1:27017/url-shortener");
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
+app.set("view engine", "ejs"); // this means we are using ejs as our view engine
+app.set("views", path.resolve("./views"));// this means all the view files are stored in views folder
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
